89b7fb8a92ad699e4e48d5a8e2f5fbe50bc3bf51
Beau Seate <rseate@uncc.edu>
2021-03-25 14:06:31 -0400

    This is for feature #12

--- Home.py
+++ Home.py
@@ -6,13 +6,15 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 
 app = Flask(__name__)     # create an app
+a_user = {'name': 'Test_User', 'email':'Test_User@uncc.edu'}
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('Home.html')
+    return render_template('Home.html', user = a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
