809fa3bd2f391d9e948289dbe3c16346a2bd2dfe
Taylor Thomas <tthom119@uncc.edu>
2021-03-31 22:53:52 -0400

    Changed error messages so they appear on the registration page instead of a new, separate page

--- Home.py
+++ Home.py
@@ -15,6 +15,7 @@ with app.app_context():
     db.create_all()
 
 logDetails = {'LoggedIn': False, 'User': None}
+errorDetails = {'HasError': False, 'Message': None}
 
 
 # @app.route is a decorator. It gives the function "index" special powers.
@@ -36,8 +37,8 @@ def registration():
         name = request.form['full_name']
         password = request.form['user_password']
         validation = validate_credentials(userEmail, name, password)
-        if validation.get('Result') is False:
-            return validation.get('Message')
+        if validation.get('HasError'):
+            return render_template('Registration.html', error = errorDetails)
         newUser = User(userEmail, name, password)
         db.session.add(newUser)
         db.session.commit()
@@ -46,33 +47,33 @@ def registration():
         logDetails['LoggedIn'] = True
         return redirect(url_for('index', user=currentUser))
     else:
-        return render_template('Registration.html')
+        return render_template('Registration.html', error = errorDetails)
+
 
 def validate_credentials(e, n, p):
-    error = ""
-    details = {'Message': error, 'Result': True}
     if str(e) == "" or str(n) == "" or str(p) == "":
-        details['Message'] = '<h1> Fields cannot be left empty. </h1>'
-        details['Result'] = False
-        return details
+        errorDetails['HasError'] = True
+        errorDetails['Message'] = 'Fields cannot be left empty.'
+        return errorDetails
     if re.search('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$', e) is None:
-        details['Message'] = '<h1> Invalid email </h1'
-        details['Result'] = False
-        return details
+        errorDetails['HasError'] = True
+        errorDetails['Message'] = 'Invalid email'
+        return errorDetails
     if re.search('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$', p) is None:
-        details['Result'] = False
-        details['Message'] = ''' <h1>
-                            Error! <br>
-                            Password must contain: <br>
-                                    At least one number <br>
-                                    At least one uppercase letter and one lowercase letter <br>
-                                    At least one special symbol <br>
-                                    Have a length between 6-20 characters
-                                    </h1>
-                        '''
+        errorDetails['HasError'] = True
+        errorDetails['Message'] = '''
+                                    Password must contain
+                                                   * At least one number,
+                                                   * At least one uppercase letter and one lowercase letter,
+                                                   * At least one special symbol,
+                                                   * Have a length between 6-20 characters
+                         '''
+
+        return errorDetails
     else:
-        details['Message'] = 'No errors'
-        return details
+        errorDetails['HasError'] = False
+        errorDetails['Message'] = ""
+        return errorDetails
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
