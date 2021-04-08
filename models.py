19250a2392028b182de7af076a007bb736dbe73e
Taylor Thomas <tthom119@uncc.edu>
2021-04-08 14:22:17 -0400

    Removed unncessary comment from User class

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class User(db.Model):
     email = db.Column("Email", db.String(50), primary_key=True)
     full_name = db.Column("Name", db.String(75))
     password = db.Column("Password", db.String(100))
-    # Need to add a column for events attending/liked
 
     def __init__(self, email, full_name, password):
         self.email = email
