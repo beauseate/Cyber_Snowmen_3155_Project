eea0626877aa569bfc417d32bc8df14664fefd33
Taylor Thomas <tthom119@uncc.edu>
2021-04-14 14:26:37 -0400

    Added an RSVP table

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     email = db.Column("Email", db.String(50))
     full_name = db.Column("Name", db.String(75))
     password = db.Column("Password", db.String(255))
+    events_attending = db.relationship("RSVP", backref="user", lazy=True)
 
     def __init__(self, email, full_name, password):
         self.email = email
@@ -22,6 +23,7 @@ class Event(db.Model):
     user = db.Column("User", db.String(50))
     reports = db.Column("Reports", db.Integer)
     desc = db.Column("Description", db.String(500))
+    events_attending = db.relationship("RSVP", backref="event",cascade="all, delete-orphan", lazy=True)
 
     def __init__(self, event_id, date, name, rating, user, reports, desc):
         self.event_id = event_id
@@ -31,3 +33,12 @@ class Event(db.Model):
         self.user = user
         self.reports = reports
         self.desc = desc
+class RSVP(db.Model):
+    RSVP_id = db.Column("RSVP_ID", db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey("event.Event_ID"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.User_ID"))
+
+    def __init__(self, id, event_id, user_id):
+        self.RSVP_id = id
+        self.event_id = event_id
+        self.user_id = user_id
