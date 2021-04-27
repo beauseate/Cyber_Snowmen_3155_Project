f970f6757edb0c9c76705e3c16046c17cd197601
Jarod <jruschil@uncc.edu>
2021-04-27 14:15:09 -0400

    added an auto deletion after a large number of reports

--- routes.py
+++ routes.py
@@ -82,7 +82,16 @@ def get_event(e_id):
                 flash("You like this event!")
                 return redirect(url_for('get_event', e_id=eventExists.event_id))
         if request.method == 'POST' and ('report' in request.form):
-            eventExists.reports = 1
+            eventExists.reports += 1
+            if eventExists.reports > 9:
+                flash("The event has been reported too many times and will now be deleted")
+                eventExists = db.session.query(Event).filter_by(event_id=eventExists.event_id).one()
+                db.session.delete(eventExists)
+                db.session.commit()
+                
+                flash("The event has been reported too many times and will now be deleted")
+                return redirect(url_for('index', events=listEvents, user=session['user']))
+                
             db.session.commit()
             flash("Event Reported!")
             return redirect(url_for('get_event', e_id=eventExists.event_id))
