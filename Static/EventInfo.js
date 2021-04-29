d7cf1983156768383180fba0fd467d3635bb4fd3
Beau Seate <rseate@uncc.edu>
2021-04-29 08:09:54 -0400

    Fixed issue when selecting 3 star rating not displaying correctly

--- Static/EventInfo.js
+++ Static/EventInfo.js
@@ -27,6 +27,7 @@ window.onload = function() {
         document.getElementById("5").style.color = "#9e9e9e"; 
         document.getElementById("4").style.color = "#9e9e9e";
         document.getElementById("3").style.color = "orange";
+        document.getElementById("2").style.color = "orange";
         document.getElementById("1").style.color = "orange";
     }
     
